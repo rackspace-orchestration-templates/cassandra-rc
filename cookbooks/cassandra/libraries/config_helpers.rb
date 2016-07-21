95cd07388a053eba27df6ea5d82ea74d531cb86f
Chris Hultin <chris.hultin@rackspace.com>
2016-07-21 10:55:29 -0500

    Initial Commit

---
+++ cookbooks/cassandra/libraries/config_helpers.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: cassandra
+# Libraries:: config_helpers
+#
+
+require 'yaml'
+
+def cassandra_bool_config(config_val)
+  if config_val.is_a?(String)
+    return config_val
+  elsif config_val
+    return 'true'
+  else
+    return 'false'
+  end
+end
+
+def hash_to_yaml_string(hash)
+  hash.to_hash.to_yaml
+end
